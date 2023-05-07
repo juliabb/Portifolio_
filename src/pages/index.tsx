f3cd92303ef408a26cf2afce1af8a2dbdca6fbb3
Julia Benedicto <68789655+juliabb@users.noreply.github.com>
2023-05-07 20:14:43 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "@styles/home.module.css";
+import styles from '@styles/home.module.css'
 import { Card } from "../components/Card/Card";
 import { Banner } from "@components/Banner/Banner";
 import { Projects } from "@constants/Projects";
